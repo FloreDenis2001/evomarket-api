95c70a689294f39331fb22db8301570720004cb7
Denis Floare <floredenis907@yahoo.com>
2023-09-12 16:01:04 +0300

    first_commit

---
+++ src/main/java/ro/mycode/evomarketapi/EvomarketApiApplication.java
@@ -0,0 +1,13 @@
+package ro.mycode.evomarketapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvomarketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvomarketApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/mycode/evomarketapi/EvomarketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.mycode.evomarketapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvomarketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
